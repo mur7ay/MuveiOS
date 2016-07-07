b23b5e71f75a293d0b3d3b4ce06a6264fccb7c00
Givi Pataridze <pataridzegivi@gmail.com>
2016-07-07 21:34:29 +0300

    separate dataSource for autocompletion

---
+++ Muve/Classes/Controllers/Map/DataSource/MapViewDataSource.swift
@@ -0,0 +1,53 @@
+//
+//  MapViewDataSource.swift
+//  Muve
+//
+//  Created by Givi Pataridze on 07/07/16.
+//  Copyright © 2016 Muve. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+
+class MapViewDataSource: NSObject {
+    
+    var searchResults: [GooglePlaces.PlaceAutocompleteResponse.Prediction] = []
+    let dataSourceTrigger: QueryFunc
+    
+    init(trigger: QueryFunc) {
+        dataSourceTrigger = trigger
+        super.init()
+    }
+
+    
+    func searchWithText(text: String) {
+        guard text != "" else {
+            searchResults = []
+            return
+        }
+        GooglePlaces.placeAutocomplete(forInput: text) { (response, error) -> Void in
+            guard response?.status == GooglePlaces.StatusCode.OK else {
+                DLog("\(response?.errorMessage)")
+                return
+            }
+            if let predictions = response?.predictions {
+                self.searchResults = predictions
+            }
+            self.dataSourceTrigger()
+            DLog("first matched result: \(response?.predictions.first?.description)")
+        }
+    }
+}
+
+extension MapViewDataSource: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(R.reuseIdentifier.autoCompletionCellID)!
+        cell.lblPlace.text = searchResults[indexPath.row].description
+        return cell
+    }
+}
--- Muve/Classes/Controllers/Map/MapViewController.swift
+++ Muve/Classes/Controllers/Map/MapViewController.swift
@@ -11,13 +11,7 @@ import GoogleMaps
 import CoreLocation
 import GooglePlaces
 
-struct RouteQuery {
-    var fromPlace: GMSPlace!
-    var toPlace: GMSPlace!
-}
-
-
-class MapViewController: UIViewController, BaseViewController {
+class MapViewController: UIViewController {
     
     private let manager = CLLocationManager()
 
@@ -26,20 +20,7 @@ class MapViewController: UIViewController, BaseViewController {
     
     private var isFromFieldActive: Bool = true
     
-    private var searchResults: [String] = [] {
-        didSet {
-            if searchResults.isEmpty {
-                tableContainer.hidden = true
-            } else {
-                tableView.reloadData()
-                tableContainer.hidden = false
-            }
-            setupTableContainerHeight(searchResults.count)
-        }
-    }
-    
-    private var searchPredictions: [GooglePlaces.PlaceAutocompleteResponse.Prediction]!
-    
+    private var dataSource: MapViewDataSource!
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet private weak var tableContainer: UIView!
@@ -47,8 +28,6 @@ class MapViewController: UIViewController, BaseViewController {
     @IBOutlet private weak var txtPickupLocation: UITextField!
     @IBOutlet private weak var txtDropOffLocation: UITextField!
     @IBOutlet private weak var btnPin: UIButton!
-
-    
 
     @IBOutlet weak var constraintTableContainerHeight: NSLayoutConstraint!
     @IBOutlet weak var constraintTxtFieldBottom: NSLayoutConstraint!
@@ -58,7 +37,6 @@ class MapViewController: UIViewController, BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupNavigationBar()
         setupCoreLocation()
         setupGoogleMap()
@@ -69,10 +47,6 @@ class MapViewController: UIViewController, BaseViewController {
     
     deinit {
         unregisterKeyboardNotifications()
-    }
-    
-    static func storyBoardName() -> String {
-        return "Map"
     }
     
     private func setupNavigationBar() {
@@ -97,24 +71,20 @@ class MapViewController: UIViewController, BaseViewController {
                 title: "Background Location Access Disabled",
                 message: "In order to be notified about moves near you, please open this app's settings and set location access to 'Always'.",
                 preferredStyle: .Alert)
-            
             let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
             alertController.addAction(cancelAction)
-            
             let openAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
                 if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
                     UIApplication.sharedApplication().openURL(url)
                 }
             }
             alertController.addAction(openAction)
-            
             self.presentViewController(alertController, animated: true, completion: nil)
         }
     }
     
     private func setupGoogleMap() {
         GooglePlaces.provideAPIKey(Maps.googleAPIKey)
-        
         let camera = GMSCameraPosition.cameraWithLatitude(39.104252, longitude: -84.515648, zoom: 10)
         map = GMSMapView.mapWithFrame(view.frame, camera: camera)
         map.delegate = self
@@ -146,29 +116,23 @@ class MapViewController: UIViewController, BaseViewController {
     }
     
     private func setupAutoCompletion() {
+        let newDataTrigger = {
+            if self.dataSource.searchResults.isEmpty {
+                self.tableContainer.hidden = true
+            } else {
+                self.tableView.reloadData()
+                self.tableContainer.hidden = false
+            }
+            self.setupTableContainerHeight(self.dataSource.searchResults.count)
+        }
+        dataSource = MapViewDataSource(trigger: newDataTrigger)
+
         tableView.delegate = self
-        tableView.dataSource = self
+        tableView.dataSource = dataSource
         tableView.separatorColor = UIColor.clearColor()
         tableView.registerNib(R.nib.autoCompletionCell)
         tableContainer.hidden = true
     }
-    
-    @IBAction func btnDropLocationPressed(sender: AnyObject) {
-        let _ = map.convertPoint(map.center, toView: view)
-        constraintTxtFieldLeading.constant = 10 - txtPickupLocation.bounds.size.width
-        UIView.animateWithDuration(0.3) {
-            self.view.layoutIfNeeded()
-        }
-    }
-
-    
-    @IBAction func btnPinPressed(sender: AnyObject) {
-        constraintTxtFieldLeading.constant = 0
-        UIView.animateWithDuration(0.3) {
-            self.view.layoutIfNeeded()
-        }
-    }
-    
     
     private func registerKeyboardNotifications() {
         NSNotificationCenter.defaultCenter().addObserver(self,
@@ -184,6 +148,25 @@ class MapViewController: UIViewController, BaseViewController {
     
     private func unregisterKeyboardNotifications() {
         NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    
+    
+    
+    @IBAction func btnDropLocationPressed(sender: AnyObject) {
+        let _ = map.convertPoint(map.center, toView: view)
+        constraintTxtFieldLeading.constant = 10 - txtPickupLocation.bounds.size.width
+        UIView.animateWithDuration(0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    
+    @IBAction func btnPinPressed(sender: AnyObject) {
+        constraintTxtFieldLeading.constant = 0
+        UIView.animateWithDuration(0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     func willKeyboardShown(notification: NSNotification) {
@@ -210,24 +193,6 @@ class MapViewController: UIViewController, BaseViewController {
         }
     }
     
-    func searchWithText(text: String) {
-        guard text != "" else {
-            searchResults = []
-            return
-        }
-        GooglePlaces.placeAutocomplete(forInput: text) { (response, error) -> Void in
-            guard response?.status == GooglePlaces.StatusCode.OK else {
-                DLog("\(response?.errorMessage)")
-                return
-            }
-            if let predictions = response?.predictions {
-                self.searchPredictions = predictions
-                self.searchResults = predictions.map({ $0.description! })
-            }
-            DLog("first matched result: \(response?.predictions.first?.description)")
-        }
-    }
-    
     private func setupTableContainerHeight(count: Int) {
         if isFromFieldActive {
             constraintTableContainerHeight.constant = CGFloat(count * 44)
@@ -238,11 +203,9 @@ class MapViewController: UIViewController, BaseViewController {
         }
         tableContainer.layoutIfNeeded()
     }
-
 }
 
 extension MapViewController: GMSMapViewDelegate {
-    
     func mapView(mapView: GMSMapView, willMove gesture: Bool) {
         recenterMarkerInMapView()
     }
@@ -262,7 +225,6 @@ extension MapViewController: GMSMapViewDelegate {
 }
 
 extension MapViewController: CLLocationManagerDelegate {
-    
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status == .AuthorizedWhenInUse {
             manager.startUpdatingLocation()
@@ -271,14 +233,11 @@ extension MapViewController: CLLocationManagerDelegate {
 }
 
 extension MapViewController: UITextFieldDelegate {
-    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         switch textField {
         case txtPickupLocation:
             btnDropLocationPressed(self)
-//            if !textField.text!.isEmpty {
-                txtPickupLocation.endEditing(true)
-//            }
+            txtPickupLocation.endEditing(true)
             return true
         case txtDropOffLocation:
             txtDropOffLocation.endEditing(true)
@@ -292,22 +251,21 @@ extension MapViewController: UITextFieldDelegate {
         if textField === txtPickupLocation {
             let newString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
             isFromFieldActive = true
-            searchWithText(newString)
+            dataSource.searchWithText(newString)
         }
         if textField === txtDropOffLocation {
             let newString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
             isFromFieldActive = false
-            searchWithText(newString)
+            dataSource.searchWithText(newString)
         }
         return true
     }
-    
 }
 
 extension MapViewController: UITableViewDelegate {
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        searchResults = []
-        let place = searchPredictions[indexPath.row]
+        dataSource.searchResults = []
+        let place = dataSource.searchResults[indexPath.row]
         if isFromFieldActive {
             txtPickupLocation.text = place.description
         } else {
@@ -316,19 +274,9 @@ extension MapViewController: UITableViewDelegate {
     }
 }
 
-extension MapViewController: UITableViewDataSource {
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(R.reuseIdentifier.autoCompletionCellID)!
-        cell.lblPlace.text = searchResults[indexPath.row]
-        return cell
+extension MapViewController: BaseViewController {
+    static func storyBoardName() -> String {
+        return "Map"
     }
 }
 
